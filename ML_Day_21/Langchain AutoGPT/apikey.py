f7f983540d15926ab4100e725f91a821aa611bf9
piyushGenzeon <136071279+piyushGenzeon@users.noreply.github.com>
2023-07-13 23:53:35 +0530

    LangChain AutoGPT

---
+++ ML_Day_21/Langchain AutoGPT/apikey.py
@@ -0,0 +1,1 @@
+apikey = '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'
---
+++ ML_Day_21/Langchain AutoGPT/app.py
@@ -0,0 +1,41 @@
+import os
+from apikey import apikey
+
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain, SimpleSequentialChain
+from langchain.memory import ConversationBufferMemory
+# from langchain.utilities import WikipediaAPIWrapper
+
+os.environ['OPENAI_API_KEY'] = apikey
+
+# App framework
+st.title('🦜🔗 YouTube GPT Creator')
+prompt = st.text_input('Plug in your prompt here')
+
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables = ['topic'],
+    template='write me a youtube video title about {topic}'
+)
+
+script_template = PromptTemplate(
+    input_variables = ['title', 'wikipedia_research'],
+    template='write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia reserch:{wikipedia_research} '
+)
+
+# # Memory
+# title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+
+
+# Llms
+llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], verbose=True)
+
+# Show stuff to the screen if there's a prompt
+if prompt:
+    response=sequential_chain.run(prompt)
+    st.write(response)
