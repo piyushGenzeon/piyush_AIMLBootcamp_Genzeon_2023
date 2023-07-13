45f254a71110671b0ea0ab715dc5619067a8a0c2
piyushGenzeon <136071279+piyushGenzeon@users.noreply.github.com>
2023-07-13 23:52:53 +0530

    LangChain with Azure OpenAI

---
+++ ML_Day_21/Langchain with Azure OpenAI/app.py
@@ -0,0 +1,6 @@
+import os
+OPENAI_API_TYPE = "Azure"
+OPENAI_API_VERSION = "2022-12-01"
+OPENAI_API_BASE = "https://channl-openai-ins.openai.azure.com/"
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+DEPLOYMENT_NAME = "text-davinci-002"
